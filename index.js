4d490f6ace5a60e7c9e55c677b7609d5bea549df
akhilmyana <132043103+akhilmyana@users.noreply.github.com>
2023-12-11 19:25:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-zconst Button = (props) => {
+const Button = (props) => {
   //  Write your code here.
   const {heading, btnText} = props
   return(
   <div className = "background">
-    <h1 className = heading>{heading}</h1>
+    <h1 className = "heading">{heading}</h1>
     <button className = {btnText}>{btnText}</button>
   </div>;
   )
