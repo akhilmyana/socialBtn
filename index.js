cde86cc8a4e8228ee6170ad1dfc3a0ec3ae61f99
akhilmyana <132043103+akhilmyana@users.noreply.github.com>
2023-12-11 19:19:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const Button = (props) => {
   //  Write your code here.
-  const {heading, btn-text} = props
-  return{
+  const {heading, btnText} = props
+  return(
   <div className = "background">
     <h1 className = heading>{heading}</h1>
-    <button className = {btn-text}>{btn-text}</button>
+    <button className = {btnText}>{btnText}</button>
   </div>;
-  }
+  )
 };
 
 const element = (
   //  Write your code here.
   <Button heading = "Social Buttons"/>
-  <Button btn-text="like"/>
-  <Button btn-text="comment"/>
-  <Button btn-text="share"/>
+  <Button btnText="like"/>
+  <Button btnText="comment"/>
+  <Button btText="share"/>
 );
 
 ReactDOM.render(element, document.getElementById("root"));
